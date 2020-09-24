0aab6f48ea08ee52a9b1fa87952093edb2dc6e71
Evgeny Ukhanov <mrlsd@ya.ru>
2020-09-24 11:26:42 +0300

    Extend examples: hello, hello_fn

--- examples/hello.i
+++ examples/hello.i
@@ -1,10 +1,4 @@
 module main
 
-let hello_str = hello_world
-
 let main args =
-    let x = a + b
-    let y = x / c
-    printfn hello_str x y
-    let z = myfunc1()
-    myfunc2()
+    printfn "Hello world"
--- src/tests/hello.rs
+++ src/tests/hello.rs
@@ -8,10 +8,8 @@ fn read_source(file: &str) -> String {
 
 #[test]
 #[allow(dead_code)]
-pub fn test_exampels_hello_world() {
-    let _src = read_source("./examples/hello.i");
-    let _x = main;
-    //let _res = main(Span::new(src.as_str())).unwrap();
-    // assert_eq!(res.0.fragment(), &"");
-    // println!("{:#?}", res);
+pub fn test_exampels_hello() {
+    let src = read_source("./examples/hello.i");
+    let res = main(Span::new(src.as_str())).unwrap();
+    assert_eq!(res.0.fragment(), &"");
 }
