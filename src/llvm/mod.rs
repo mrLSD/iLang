5f3de7f92a69e9ab2d6441e1e121fa061f8c434a
Evgeny Ukhanov <mrlsd@ya.ru>
2020-10-02 22:04:11 +0300

    LLVM: add Visibility Styles

--- src/llvm/mod.rs
+++ src/llvm/mod.rs
@@ -1,3 +1,4 @@
 pub mod calling_convention;
 pub mod linkage_types;
 pub mod parameter_attributes;
+pub mod visibility_styles;
---
+++ src/llvm/visibility_styles.rs
@@ -0,0 +1,26 @@
+//! # Visibility Styles
+//!
+//! All Global Variables and Functions have one of the following
+//! visibility styles
+//!
+//! A symbol with internal or private linkage must have default visibility.
+//!
+//! https://llvm.org/docs/LangRef.html#id1246
+
+pub enum VisibilityStyles {
+    Default,
+    Hidden,
+    Protected,
+}
+
+impl std::fmt::Display for VisibilityStyles {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let s = match self {
+            VisibilityStyles::Default => "default",
+            VisibilityStyles::Hidden => "hidden",
+            VisibilityStyles::Protected => "protected",
+        };
+
+        write!(f, "{}", s)
+    }
+}
