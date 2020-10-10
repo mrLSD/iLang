5b8dcb7fd153db04344d07d9406a6d0d92fcd4c6
Evgeny Ukhanov <mrlsd@ya.ru>
2020-10-10 18:29:59 +0300

    LLVM: Global vals IR generator

--- src/llvm/global_variables.rs
+++ src/llvm/global_variables.rs
@@ -5,6 +5,17 @@
 //!
 //! Global variable definitions must be initialized.
 //!
+//! Syntax:
+//! ```html
+//! @<GlobalVarName> = [Linkage] [PreemptionSpecifier] [Visibility]
+//!                    [DLLStorageClass] [ThreadLocal]
+//!                    [(unnamed_addr|local_unnamed_addr)] [AddrSpace]
+//!                    [ExternallyInitialized]
+//!                    <global | constant> <Type> [<InitializerConstant>]
+//!                    [, section "name"] [, comdat [($name)]]
+//!                    [, align <Alignment>] (, !name !N)*
+//! ```
+//!
 //! https://llvm.org/docs/LangRef.html#global-variables
 
 use super::{
@@ -75,7 +86,45 @@ impl std::fmt::Display for GlobalVariableKind {
 
 impl<T: std::fmt::Display> std::fmt::Display for GlobalVariable<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let s = "";
+        let mut s = format!("@{} = ", self.name);
+        if self.linkage.is_some() {
+            s = format!("{} {} ", s, self.linkage.as_ref().unwrap());
+        }
+        if self.preemption_specifier.is_some() {
+            s = format!("{} {} ", s, self.preemption_specifier.as_ref().unwrap());
+        }
+        if self.visibility.is_some() {
+            s = format!("{} {} ", s, self.visibility.as_ref().unwrap());
+        }
+        if self.dll_storage_classes.is_some() {
+            s = format!("{} {} ", s, self.dll_storage_classes.as_ref().unwrap());
+        }
+        if self.thread_local.is_some() {
+            s = format!("{} {} ", s, self.thread_local.as_ref().unwrap());
+        }
+        if self.unnamed_addr.is_some() {
+            s = format!("{} {} ", s, self.unnamed_addr.as_ref().unwrap());
+        }
+        if self.addrspace.is_some() {
+            s = format!("{} {} ", s, self.addrspace.as_ref().unwrap());
+        }
+        s = format!("{} {} {} ", s, self.global_variable_kind, self.value_type);
+        if self.initializer_constant.is_some() {
+            s = format!("{} {} ", s, self.initializer_constant.as_ref().unwrap());
+        }
+        if self.section.is_some() {
+            s = format!("{}, {} ", s, self.section.as_ref().unwrap());
+        }
+        if self.comdat.is_some() {
+            s = format!("{}, {} ", s, self.comdat.as_ref().unwrap());
+        }
+        if self.alignment.is_some() {
+            s = format!("{}, {} ", s, self.alignment.as_ref().unwrap());
+        }
+        if self.metadata.is_some() {
+            s = format!("{}, {} ", s, self.metadata.as_ref().unwrap());
+        }
+
         write!(f, "{}", s)
     }
 }
--- src/llvm/types.rs
+++ src/llvm/types.rs
@@ -2,3 +2,10 @@
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct Type;
+
+impl std::fmt::Display for Type {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let s = "";
+        write!(f, "{}", s)
+    }
+}
