8aceb418ad4290b24a84fcb66f805de4eb379902
Evgeny Ukhanov <mrlsd@ya.ru>
2020-10-15 22:27:19 +0300

    LLVM instructions: switch

--- src/llvm/type_system/instructions/mod.rs
+++ src/llvm/type_system/instructions/mod.rs
@@ -24,6 +24,11 @@ impl<T: std::fmt::Display> std::fmt::Display for Ret<T> {
 /// different basic block in the current function. There are two forms of
 /// this instruction, corresponding to a conditional branch and an
 /// unconditional branch.
+/// Syntax:
+/// ```html
+/// br i1 <cond>, label <iftrue>, label <iffalse>
+/// br label <dest>
+/// ```
 /// https://llvm.org/docs/LangRef.html#br-instruction
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub enum Br {
@@ -35,10 +40,46 @@ impl std::fmt::Display for Br {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let s = match self {
             Br::Conditional(cond, if_true, if_false) => {
-                format!("%{} %{} %{}", cond, if_true, if_false)
+                format!("i1 %{}, label %{}, label %{}", cond, if_true, if_false)
             }
-            Br::Unconditional(dest) => format!("%{}", dest),
+            Br::Unconditional(dest) => format!("label %{}", dest),
         };
         write!(f, "br {}", s)
+    }
+}
+
+/// The ‘switch’ instruction is used to transfer control flow to one of
+/// several different places. It is a generalization of the ‘br’
+/// instruction, allowing a branch to occur to one of many possible destinations.
+/// Syntax:
+/// ```html
+/// switch <intty> <value>, label <defaultdest> [ <intty> <val>, label <dest> ... ]
+/// ```
+/// https://llvm.org/docs/LangRef.html#switch-instruction
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Switch {
+    choices: Vec<(String, String)>,
+}
+
+impl std::fmt::Display for Switch {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let s = self
+            .choices
+            .iter()
+            .enumerate()
+            .fold("".to_string(), |s, (i, (int_ty, label))| {
+                // First item
+                let s = if i < 1 {
+                    format!("i32 {}, label %{} [\n", int_ty, label)
+                } else {
+                    format!("{} i32 {}, label %{}\n", s, int_ty, label)
+                };
+                if i + 1 == self.choices.len() {
+                    format!("{} \n]", s)
+                } else {
+                    s
+                }
+            });
+        write!(f, "switch {}", s)
     }
 }
