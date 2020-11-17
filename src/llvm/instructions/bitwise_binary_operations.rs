df7bafb9583a5a4e282e292109afa17f98493c54
Evgeny Ukhanov <mrlsd@ya.ru>
2020-11-17 19:56:18 +0200

    LLVM instructions: lshl

--- src/llvm/instructions/bitwise_binary_operations.rs
+++ src/llvm/instructions/bitwise_binary_operations.rs
@@ -1,15 +1,15 @@
 //! # Bitwise Binary Operations
-use crate::llvm::types::Type;
+//!
+//! Bitwise binary operators are used to do various forms of
+//! bit-twiddling in a program. They are generally very efficient
+//! instructions and can commonly be strength reduced from other
+//! instructions. They require two operands of the same type, execute
+//! an operation on them, and produce a single value. The resulting
+//! value is the same type as its operands.
+//!
+//! https://llvm.org/docs/LangRef.html#bitwise-binary-operations
 
-///
-/// Bitwise binary operators are used to do various forms of
-/// bit-twiddling in a program. They are generally very efficient
-/// instructions and can commonly be strength reduced from other
-/// instructions. They require two operands of the same type, execute
-/// an operation on them, and produce a single value. The resulting
-/// value is the same type as its operands.
-///
-/// https://llvm.org/docs/LangRef.html#bitwise-binary-operations
+use crate::llvm::types::Type;
 
 /// The ‘shl’ instruction returns the first operand shifted to the
 /// left a specified number of bits.
@@ -28,6 +28,23 @@ pub struct Shl {
     pub op2: String,
 }
 
+/// The ‘lshr’ instruction (logical shift right) returns the first
+/// operand shifted to the right a specified number of bits with zero
+/// fill.
+///
+/// Both arguments to the ‘lshr’ instruction must be the same integer
+/// or vector of integer type. ‘op2’ is treated as an unsigned value.
+///
+/// https://llvm.org/docs/LangRef.html#lshr-instruction
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct LShl {
+    pub result: String,
+    pub exact: Option<()>,
+    pub ty: Type,
+    pub op1: String,
+    pub op2: String,
+}
+
 impl std::fmt::Display for Shl {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let mut s = "shl".to_string();
@@ -36,6 +53,17 @@ impl std::fmt::Display for Shl {
         }
         if self.nsw.is_some() {
             s = format!("{} nsw", s)
+        }
+        s = format!("{} {} {}, {}", s, self.ty, self.op1, self.op2);
+        write!(f, "{}", s)
+    }
+}
+
+impl std::fmt::Display for LShl {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut s = "lshl".to_string();
+        if self.exact.is_some() {
+            s = format!("{} exact", s)
         }
         s = format!("{} {} {}, {}", s, self.ty, self.op1, self.op2);
         write!(f, "{}", s)
